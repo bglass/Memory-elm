fbff29f17798c6c9d6935257b8bf73ec02de7b3c
Boris Glass <boris@glass.li>
2018-05-24 14:50:31 +0200

    grid layout

--- src/Main.elm
+++ src/Main.elm
@@ -8,6 +8,6 @@ import Subscriptions
 main = Html.program
   { init          = Init.dummy
   , update        = update
-  , view          = View.grid
+  , view          = View.main_grid
   , subscriptions = Subscriptions.none
   }
